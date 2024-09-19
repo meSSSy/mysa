34f3bade8f73665ae825de61786aa446f0b0af20
Chris Parry <chrisparry07@live.co.uk>
2024-09-19 14:36:45 +0000

    Deployment Complete Again

--- mysa_project/settings.py
+++ mysa_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-messsy-mysa-4cq5hvl08dw.ws.codeinstitute-ide.net', 'https://mysa-cbc157c11393.herokuapp.com/']
+ALLOWED_HOSTS = ['8000-messsy-mysa-4cq5hvl08dw.ws.codeinstitute-ide.net', 'mysa-cbc157c11393.herokuapp.com']
 
 
 # Application definition
