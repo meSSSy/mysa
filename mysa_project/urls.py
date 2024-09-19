ffefccf6c7e47236be4f89ddf7d8741681ec0c90
Chris Parry <chrisparry07@live.co.uk>
2024-09-19 13:30:05 +0000

    Deployment Complete

--- mysa_project/urls.py
+++ mysa_project/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from price_list import views as index_views
+from main import views as index_views
 
 urlpatterns = [
-    path('price_list/', index_views.index, name='index'),
+    path('main/', index_views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('', include('my_app.urls'), name='home'),
 ]
