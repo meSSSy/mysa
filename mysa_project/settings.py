5f2c17a411bcdc49c4a89bf9c8b89d763992bd3d
Chris Parry <chrisparry07@live.co.uk>
2024-09-24 19:09:24 +0000

    Register form added and styled using crispy_forms_bootstrap

--- mysa_project/settings.py
+++ mysa_project/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'register_page',
     'cloudinary_storage',
     'cloudinary',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -141,3 +143,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://*.ws.codeinstitute-ide.net']
+
+CRISPY_TEMPLATE_PACK="bootstrap4"
--- register_page/views.py
+++ register_page/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from .forms import RegisterForm
+
 
 def register_page(request):
     return render(request, 'register.html')
+
+def register_page(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect("/main_page")
+    
+    else:
+        form = RegisterForm()
+
+    return render(response, "register.html", {"form":form})
