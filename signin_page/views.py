3c0c2faa7795b84932c8bd4680e542e4920f145d
Chris Parry <chrisparry07@live.co.uk>
2024-09-26 10:27:46 +0000

    signin form added

--- signin_page/views.py
+++ signin_page/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from signin_page.forms import LoginForm
 
 def signin_page(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/main_page")  # Redirect to main page after successful login
+            else:
+                form.add_error(None, "Invalid username or password")
+    else:
+        form = LoginForm()
+
+    return render(request, "login.html", {"form": form})
