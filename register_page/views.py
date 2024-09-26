ab6c2e926bb76c42c12173e8766e92b640c939f2
Chris Parry <chrisparry07@live.co.uk>
2024-09-26 13:34:38 +0000

    fixed register and signin bug. site was navigating to main page when passwords didn't match or without a registered account

--- register_page/views.py
+++ register_page/views.py
@@ -1,20 +1,15 @@
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
+from django.contrib.auth import login
 from .forms import RegisterForm
 
-
 def register_page(request):
-    return render(request, 'register.html')
-
-def register_page(response):
-    if response.method == "POST":
-        form = RegisterForm(response.POST)
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-
-        return redirect("/main_page")
-    
+            user = form.save()
+            login(request, user) 
+            return redirect("/main_page")
     else:
         form = RegisterForm()
 
-    return render(response, "register.html", {"form":form})
+    return render(request, "register.html", {"form": form})
