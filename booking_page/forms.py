426ef0732a62f3d16fe1566bea7bbd8a81570eba
Chris Parry <chrisparry07@live.co.uk>
2024-09-30 18:18:09 +0000

    Social media links added to all pages diverting to mysa pages

--- booking_page/forms.py
+++ booking_page/forms.py
@@ -1,11 +1,34 @@
 from django import forms
-from .models import Appointments
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .models import Booking
 
-class AppointmentForm(forms.ModelForm):
-    class Meta:
-        model = Appointments
-        fields = ['treatment', 'day', 'time']
+class BookingForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
-        super(AppointmentForm, self).__init__(*args, **kwargs)
-        self.fields['day'].widget.attrs['type'] = 'date'
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+        if user:
+            self.fields['email'].initial = user.email
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+        self.fields['date'].required = True
+        self.fields['time'].required = True
+        self.fields['treatment'].required = True
+
+        self.fields['date'].widget = forms.DateInput(attrs={'type': 'date'})
+
+    def clean(self):
+        cleaned_data = super().clean()
+        date = cleaned_data.get('date')
+        time = cleaned_data.get('time')
+
+        if (date and time and
+                Booking.objects.filter(date=date, time=time).exists()):
+            self.add_error('time', 'We are sorry, this date and time is already booked.')
+
+    class Meta:
+        model = Booking
+        fields = [
+            'first_name', 'last_name', 'email', 'treatment', 'date', 'time',
+        ]
